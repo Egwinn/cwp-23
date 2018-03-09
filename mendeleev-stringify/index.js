cfa05dda2a1488ae5a069cff25bae531a76e994c
Marina <dubovik.m14@gmail.com>
2018-03-09 23:17:59 +0200

    import mendeleev-package and use mendeleev-function

---
+++ mendeleev-stringify/index.js
@@ -0,0 +1,9 @@
+const mendeleev = require('mendeleev');
+
+module.exports = (element) => {
+    let elem = mendeleev(element);
+    if (elem) {
+        return elem.number + '. ' +  elem.name + ' / ' + elem.weight;
+    }
+    else throw new Error('Element doesn\'t exits');
+};
